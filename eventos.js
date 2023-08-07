beb8d7224f530651913ff8d12558b94eaf9854b3
alxrolande <141145424+AlxRolande@users.noreply.github.com>
2023-08-07 11:39:00 -0300

    agrego evento js

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+const miDiv = document.getElementById("boton");
+miDiv.addEventListener("click", () => {
+    alert("Hola! Soy el div");
+});
+ });
